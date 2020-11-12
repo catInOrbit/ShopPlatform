7349b8901a467cecd652f49071f0dd4a1574be54
catInOrbit <magnetocat01@gmail.com>
2020-11-12 23:39:04 +0700

    commit

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,46 +1,6 @@
 class User {
   String _gender, name, _address, _email, _username, _phoneNumber, _shopID, _documentReference, _avatarPicURL;
 
-  String get gender => _gender;
-
-  set gender(String value) {
-    _gender = value;
-  }
-
-
-  User(this._gender, this.name, this._address, this._email, this._username,
-      this._phoneNumber, this._shopID,
-      this._avatarPicURL);
-
-  Map<String, dynamic> toJson() => _UserToJson(this);
-
-
-  User.fromJson(Map<String, dynamic> json) {
-    _shopID = json['shopID'];
-    _gender = json['gender'];
-    name = json['name'];
-    _address = json['address'];
-    _email = json['email'];
-    _username = json['username'];
-    _phoneNumber = json['phoneNumber'];
-
-    _avatarPicURL = json['avatarPicURL'];
- 
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['gender'] = this._gender;
-    data['name'] = this.name;
-    data['address'] = this._address;
-    data['email'] = this._email;
-    data['username'] = this._username;
-    data['phoneNumber'] = this._phoneNumber;
-    data['shopID'] = this._shopID;
-    data['documentReference'] = this._documentReference;
-    data['avatarPicURL'] = this._avatarPicURL;
-    return data;
-  }
-
   get address => _address;
 
   get avatarPicURL => _avatarPicURL;
@@ -81,6 +41,44 @@ class User {
 
   set address(value) {
     _address = value;
+  }
+  String get gender => _gender;
+
+  set gender(String value) {
+    _gender = value;
+  }
+
+
+  User(this._gender, this.name, this._address, this._email, this._username,
+      this._phoneNumber, this._shopID,
+      this._avatarPicURL);
+
+
+  User.fromJson(Map<String, dynamic> json) {
+    _shopID = json['shopID'];
+    _gender = json['gender'];
+    name = json['name'];
+    _address = json['address'];
+    _email = json['email'];
+    _username = json['username'];
+    _phoneNumber = json['phoneNumber'];
+
+    _avatarPicURL = json['avatarPicURL'];
+
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['gender'] = this._gender;
+    data['name'] = this.name;
+    data['address'] = this._address;
+    data['email'] = this._email;
+    data['username'] = this._username;
+    data['phoneNumber'] = this._phoneNumber;
+    data['shopID'] = this._shopID;
+    data['documentReference'] = this._documentReference;
+    data['avatarPicURL'] = this._avatarPicURL;
+    return data;
   }
 }
 
